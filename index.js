07ef61b251cea64bc6c40e1a6f6ac17c50cb2b9c
Jason Rothgeb <jrothgeb@gmail.com>
2022-08-21 16:15:06 -0500

    Initial code from previous attempts

---
+++ index.js
@@ -0,0 +1,14 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+function ShowHideDiv() {
+    if(document.getElementById("masterpiece").checked; {
+        dispatchEvent.display("Booo!")
+    }
+    var dvtext = document.getElementById("meh");
+    var dvtext = document.getElementById("blah");
+    dvtext.style.display = chkYes.checked = "Dont pick me!" : "Pick me!";
+    chkYes.style.display = dvtext.checked = "Thanks!";
+}
+
